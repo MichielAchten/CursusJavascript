c752c246ea273ef2c950e4937a54953132f4044e
MichielAchten <michiel.achten@gmail.com>
2016-11-23 15:59:06 +0100

    ProjectCookieBank1

--- CursusJavascript/Projecten/nuttig_lib.js
+++ CursusJavascript/Projecten/nuttig_lib.js
@@ -85,3 +85,49 @@ function dagAanduiden(oDatum, CSS_Class) {
     }
 }
 
+/**************** cookies ****************/
+
+function setCookie(naam, waarde, dagen) {
+    /*
+    plaats een cookie
+    naam: cookienaam;
+    waarde: optioneel, het aantal dagen dat het cookie geldig blijft vanaf nu
+        indien afwezig wordt het een session cookie
+    */
+
+    var verval = "";
+    if (dagen) {
+        //vandaag global bovenaan deze lib;
+        var vervalDatum = new Date(vandaag.getTime() + dagen * 24 * 60 * 60 * 1000);
+        verval = vervalDatum.toUTCString();
+    }
+    document.cookie = naam + "=" + waarde + ";expires=" + verval;
+}
+//----------------------------------------
+function getCookie(naam) {
+    /*
+    leest een cookie
+    naam: cookienaam
+    */
+    var zoek = naam + "=";
+    if (document.cookie.length > 0) {
+        var begin = document.cookie.indexOf(zoek);
+        if (begin != -1) {
+            begin += zoek.length;
+            var einde = document.cookie.indexOf(";", begin);
+            if (einde == -1) {
+                einde = document.cookie.length;
+            }
+            return document.cookie.substring(begin, einde);
+        }
+    }
+}
+//----------------------------------------
+function clearCookie(naam) {
+    /*
+    verwijdert een cookie
+    naam: cookienaam
+    */
+    setCookie(naam, "", -1);
+}
+
