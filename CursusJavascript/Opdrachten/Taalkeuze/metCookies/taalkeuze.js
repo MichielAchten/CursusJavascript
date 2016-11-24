ac3142434a1d496f5da7de5738264008397b0e70
MichielAchten <michiel.achten@gmail.com>
2016-11-24 15:55:40 +0100

    OpdrachtTaalkeuzeMetCookies1

---
+++ CursusJavascript/Opdrachten/Taalkeuze/metCookies/taalkeuze.js
@@ -0,0 +1,25 @@
+﻿/* TAALKEUZE */
+
+
+
+function kiesTaal(eKeuze) {
+    var sKeuze = eKeuze.id;
+    switch (sKeuze) {
+        case "nederlands":
+            setCookie("taal", "nederlands", 100);
+            break;
+        case "francais":
+            setCookie("taal", "francais", 100);
+            break;
+        case "english":
+            setCookie("taal", "english", 100);
+            break;
+    }
+    window.history.go(0);
+}
+
+function verwijderTaalkeuze() {
+    clearCookie("taal");
+    var url = "intropagina.html";
+    top.location.href = url;
+}
