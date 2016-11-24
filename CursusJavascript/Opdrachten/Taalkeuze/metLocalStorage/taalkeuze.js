10b37a54b5a54eba93c3620512173fe35fdd2c38
MichielAchten <michiel.achten@gmail.com>
2016-11-24 15:38:44 +0100

    OpdrachtTaalkeuzeMetLocalStorage1

---
+++ CursusJavascript/Opdrachten/Taalkeuze/metLocalStorage/taalkeuze.js
@@ -0,0 +1,25 @@
+﻿/* TAALKEUZE */
+
+
+
+function kiesTaal(eKeuze) {
+    var sKeuze = eKeuze.id;
+    switch (sKeuze) {
+        case "nederlands":
+            localStorage.setItem("taal", "nederlands");
+            break;
+        case "francais":
+            localStorage.setItem("taal", "francais");
+            break;
+        case "english":
+            localStorage.setItem("taal", "english");
+            break;
+    }
+    window.history.go(0);
+}
+
+function verwijderTaalkeuze() {
+    localStorage.clear();
+    var url = "intropagina.html";
+    top.location.href = url;
+}
